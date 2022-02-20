a298b06c9b98e291349040b11f07f172b01ba44d
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-02-20 17:58:15 +0100

    start Fetching DATA FROM API Entpoint

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
+let blockquote = document.getElementById("advice");
+// blockquote.innerText = "";
+
 const URL = "https://api.adviceslip.com";
-const Params = {
+const params = {
+  //* ENDPOINTS
   random: "/advice",
   byID: "/advice/{slip_id}",
   bySearch: "/advice/search/{query}",
 };
+
+const getAdvice = async (url, endpoint) => {
+  let querry = endpoint;
+  endpoint = url + endpoint;
+  fetch(endpoint);
+  await ((response) => {
+    console.log(response.json());
+  });
+};
+const updateAdviceText = (data) => {
+  console.log("DATA: ", data);
+};
+
+let randomAdvice = getAdvice(URL, params.random);
